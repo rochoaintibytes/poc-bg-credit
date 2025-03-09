e0820c99eba81f791a7fb28bd13af06e9ada7ad8
coBearCoding <cobearcoding@gmail.com>
2025-03-09 00:33:58 -0500

    [add]: card, checkbox, input, navbar

---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className="w-screen flex items-center gap-2 h-18 bg-[#d2006e]">
+      <img src="/logo_bg.svg" />
+      <span className="text-white font-bold">Banco Guayaquil</span>
+    </div>
+  );
+};
+
+export { Navbar };
--- src/pages/credit/index.tsx
+++ src/pages/credit/index.tsx
@@ -1,9 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import { Navbar } from "@/components/ui/navbar";
 
 export default function CreditPage() {
   return (
-    <div className="bg-white h-screen justify-center flex items-center">
-      <Button>Primer button</Button>
-    </div>
+    <>
+      <Navbar />
+      <div className="bg-white h-screen justify-center flex items-center">
+        <Card className="p-4 min-h-4/12 min-w-4/12 justify-center flex flex-col gap-16">
+          <CardTitle>Analizador de créditos</CardTitle>
+          <CardDescription>
+            <h2>Conoce tu perfil de crédito al instante</h2>
+          </CardDescription>
+          <Button>Primer button</Button>
+        </Card>
+      </div>
+    </>
   );
 }
