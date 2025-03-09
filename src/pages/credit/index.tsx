38a92bec2d23f09a8a789ba059faa171e7e358fc
coBearCoding <cobearcoding@gmail.com>
2025-03-09 00:52:04 -0500

    [add]: input + card + button + input

--- src/pages/credit/index.tsx
+++ src/pages/credit/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import { Navbar } from "@/components/ui/navbar";
 
 export default function CreditPage() {
@@ -9,8 +10,14 @@ export default function CreditPage() {
       <div className="bg-white h-screen justify-center flex items-center">
         <Card className="p-4 min-h-4/12 min-w-4/12 justify-center flex flex-col gap-16">
           <CardTitle>Analizador de créditos</CardTitle>
-          <CardDescription>
-            <h2>Conoce tu perfil de crédito al instante</h2>
+          <CardDescription className="gap-16 flex flex-col">
+            <h2 className="text-center">
+              Conoce tu perfil de crédito al instante
+            </h2>
+            <Input
+              leadingTitle="Cédula de identidad o ruc"
+              placeholder="Ej: 0926480146001"
+            />
           </CardDescription>
           <Button>Primer button</Button>
         </Card>
