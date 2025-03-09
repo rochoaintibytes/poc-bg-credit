a60287966155aa2455ce7b12fe5ac0008a5b36fe
coBearCoding <cobearcoding@gmail.com>
2025-03-08 23:23:59 -0500

    [add]: button

---
+++ src/pages/credit/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function CreditPage() {
+  return (
+    <div className="bg-white h-screen justify-center flex items-center">
+      <Button>Primer button</Button>
+    </div>
+  );
+}
